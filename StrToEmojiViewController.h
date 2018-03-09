cf909040e584f45a97f43b77ebc6549c0d1dafe2
ydd <1067221279@qq.com>
2018-03-09 10:38:41 +0800

    字符串转emoji

---
+++ StrToEmojiViewController.h
@@ -0,0 +1,13 @@
+//
+//  StrToEmojiViewController.h
+//  Yddworkspace
+//
+//  Created by ispeak on 2018/3/9.
+//  Copyright © 2018年 QH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StrToEmojiViewController : UIViewController
+
+@end
