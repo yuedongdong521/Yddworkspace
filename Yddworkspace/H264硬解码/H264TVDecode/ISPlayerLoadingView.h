c0388d2d646cfdc8f5a6b06c9e6849b7bf5f4978
yuedongdong521 <1067221279@qq.com>
2018-09-06 17:28:13 +0800

    h264硬解优化

---
+++ Yddworkspace/H264硬解码/H264TVDecode/ISPlayerLoadingView.h
@@ -0,0 +1,18 @@
+//
+//  ISPlayerLoadingView.h
+//  ViewsTalk
+//
+//  Created by ydd on 2018/9/4.
+//  Copyright © 2018年 ywx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ISPlayerLoadingView : UIView
+
+- (void)startLoading:(NSString *)tipStr;
+
+- (void)stopLoading;
+
+
+@end
