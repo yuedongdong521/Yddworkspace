940d1d620332979a6c30ebc65aa37356be425f81
yuedongdong521 <1067221279qq.com>
2020-01-15 17:34:11 +0800

    drawRect练习

---
+++ Yddworkspace/DrawView/DrawListViewController.m
@@ -0,0 +1,32 @@
+//
+//  DrawListViewController.m
+//  Yddworkspace
+//
+//  Created by ydd on 2020/1/15.
+//  Copyright © 2020 QH. All rights reserved.
+//
+
+#import "DrawListViewController.h"
+
+@interface DrawListViewController ()
+
+@end
+
+@implementation DrawListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Yddworkspace/DrawView/DrawView.m
@@ -0,0 +1,21 @@
+//
+//  DrawView.m
+//  Yddworkspace
+//
+//  Created by ydd on 2020/1/15.
+//  Copyright © 2020 QH. All rights reserved.
+//
+
+#import "DrawView.h"
+
+@implementation DrawView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
